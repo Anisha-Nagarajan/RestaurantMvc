0b817552dcf2cf2044c64927e0976db92ef8fe80
Anisha-Nagarajan <anisha99usa@gmail.com>
2020-02-20 08:12:24 +0530

    Radio Button

--- RestaurantManagementRepository/CustomerRepository.cs
+++ RestaurantManagementRepository/CustomerRepository.cs
@@ -13,10 +13,10 @@ namespace RestaurantManagementRepository
         static CustomerRepository()
         {
             CustomerEntity customer;
-            customer = new CustomerEntity(1,"Akhil","aki123","akhilnagarajan@gmail.com","Male","Anbu Nagar","Salem",8903723179);
-            CustomerDetails.Add(customer);
-            customer = new CustomerEntity(2,"Saranya", "saran123","saranyanatarajan@gmail.com", "Female", "Gandhi Nagar", "Salem",8903723179);
-            CustomerDetails.Add(customer);
+            //customer = new CustomerEntity(1,"Akhil","aki123","akhilnagarajan@gmail.com","Male","Anbu Nagar","Salem",8903723179);
+            //CustomerDetails.Add(customer);
+            //customer = new CustomerEntity(2,"Saranya", "saran123","saranyanatarajan@gmail.com", "Female", "Gandhi Nagar", "Salem",8903723179);
+            //CustomerDetails.Add(customer);
         }
         public static IEnumerable<CustomerEntity> Customers()
         {
